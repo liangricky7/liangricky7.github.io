0bd0d8b2c2908b56f5c056647d9e79b89cb4567d
liangricky7 <liangricky7@gmail.com>
2024-12-27 16:28:04 -0600

    Added Plugins and Fonts

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/home.css";
+
+const Home = () => {
+    return (
+        <>
+            <div className="">
+
+            </div>
+        </>
+    )
+};
+
+export default Home;
