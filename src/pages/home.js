20f1897de3ef94be41661f7f117c4489a2ac3dd5
liangricky7 <liangricky7@gmail.com>
2024-12-27 20:53:47 -0600

    Landing Page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "../styles/home.css";
 
+import RickyPic from "../assets/ricky.png"
+
 const Home = () => {
     return (
         <>
-            <div className="">
-
-            </div>
+            <main className="home">
+                <section className="introSection">
+                    <div className="introSectionText">
+                        <h1>👋 Hi, I'm <span className="highlight">Ricky Liang!</span></h1>
+                        <p className="subtitle">Back End Developer and Hobbyist Front End</p>
+                    </div>
+                    
+                    <img src={RickyPic} alt="Ricky Liang" className="selfPortrait"/>
+                </section>
+            </main>
         </>
     )
 };
