adf0644fa1b1e8de4411d23b63063e83a9599ec9
Ricky Liang <liangricky7@gmail.com>
2026-03-01 15:15:00 -0600

    feat: remove lazy loading

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,19 +1,15 @@
-import { lazy, Suspense } from "react"
 import { Routes, Route, Navigate } from "react-router-dom"
 // Public pages
-const Home = lazy(() => import('./pages/Home.jsx'))
-
+import Home from './pages/Home'
 export const AppRoutes = () => {    
     return (
         <>
-            <Suspense fallback={null}>
-                <Routes>
-                    {/* public pages */}
-                    <Route path="/" element={<Home />} />
-                    {/* the else statement (wow) */}
-                    <Route path="*" element={<Navigate to="/" replace/>}/>
-                </Routes>
-            </Suspense>   
+            <Routes>
+                {/* public pages */}
+                <Route path="/" element={<Home />} />
+                {/* the else statement (wow) */}
+                <Route path="*" element={<Navigate to="/" replace/>}/>
+            </Routes>
         </>
     )
 }
