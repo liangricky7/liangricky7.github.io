940cdcf65671faf067e676963ce09c2057d8e325
Ricky Liang <liangricky7@gmail.com>
2026-03-01 19:01:29 -0600

    feat: basic home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,13 +9,26 @@ gsap.registerPlugin(ScrollTrigger);
 import Headshot from "../assets/headshot.jpg"
 
 const Home = () => {
-
     useGSAP(() => {
 
     }, []);
     return (
-        <main>
-            <h1 className="">test</h1>
+        <main className="w-[var(--common-width)] pt-[calc(65px+2rem)] mx-auto">
+            <div className=" h-[calc(100vh-65px-2rem-3rem)] mt-6">
+                <div className="h-full flex gap-8">
+                    <div className="flex-5 pl-8 pt-8">
+                        <h1 className="">Welcome to my Website!</h1>
+                        <p>
+                            I'm a software engineer based in Baton Rouge, Louisiana. I have a passion for building intuitive and efficient software solutions that make a positive impact on people's lives. With experience in full-stack development, I enjoy working on projects that challenge me to learn and grow as a developer. In my free time, I love exploring new technologies, contributing to open-source projects, and collaborating with other developers to create innovative software applications.
+                        </p>
+                    </div>
+                    <div className="h-full flex-3 border-4 border-[var(--black)]">
+
+                    </div>
+                </div>
+            </div>
+
+
         </main>
     );
 };
